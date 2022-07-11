35a8eadc9ca435257ab378e0fb3b239d0ccd64ea
Kapil-Padampalle <kapilpadampalle8698@gmail.com>
2022-07-11 19:56:09 +0530

    "First-Commit"

---
+++ SpringBoot-curd-hibernate-example/src/main/java/com/springboot/SpringBootCurdHibernateExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCurdHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCurdHibernateExampleApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-curd-hibernate-example/src/main/java/com/springboot/model/Product.java
@@ -0,0 +1,43 @@
+package com.springboot.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "price")
+	private BigDecimal price;
+	
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@CreationTimestamp
+	private Date updatedAt;
+}
